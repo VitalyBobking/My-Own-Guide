ab3d37afabec7a67e8d7520ff366c6e630b5f727
VitalyBobking <bobking.vitaly.1987@gmail.com>
2018-06-21 11:50:02 +0300

    release

--- app/src/main/java/diploma/edu/zp/guide_my_own/camera2/Camera2BasicFragment.java
+++ app/src/main/java/diploma/edu/zp/guide_my_own/camera2/Camera2BasicFragment.java
@@ -70,7 +70,9 @@ import java.util.concurrent.TimeUnit;
 
 import diploma.edu.zp.guide_my_own.R;
 import diploma.edu.zp.guide_my_own.view.AutoFitTextureView;
-
+/*
+ *created by Vitaly
+ */
 
 public  class Camera2BasicFragment extends Fragment
         implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
